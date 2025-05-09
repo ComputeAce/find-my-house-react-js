5e7b362af9271a1311c4566c74628bb65c71a003
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2025-05-09 15:55:33 +0200

    Agents route

---
+++ src/app/agents/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Navbar from "../../../componets/Navbar";
+import Hero from "../../../componets/Hero";
+import Houses from "../../../componets/Houses";
+import SidebarLeft from "../../../componets/SidebarLeft";
+import SidebarRight from "../../../componets/SidebarRigh";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      {/* Main Layout */}
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+
+          {/* Left Sidebar */}
+          <div className="hidden md:block col-span-1">
+            <SidebarLeft />
+          </div>
+
+          {/* Main Content */}
+          <div className="col-span-2">
+            <Houses />
+          </div>
+
+          {/* Right Sidebar */}
+          <div className="hidden md:block col-span-1">
+            <SidebarRight />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
